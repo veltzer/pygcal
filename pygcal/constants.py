0d2fc4235cfb061c1a2e29c48e89307d6da45ccf
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-26 02:25:16 +0300

    passed lint

--- pygcal/constants.py
+++ pygcal/constants.py
@@ -5,3 +5,5 @@ constants.py
 SCOPES = [
     "https://www.googleapis.com/auth/calendar",
 ]
+API_SERVICE_NAME = "calendar"
+API_VERSION = "v3"
--- pygcal/main.py
+++ pygcal/main.py
@@ -3,18 +3,33 @@ main entry point to the program
 """
 
 import pylogconf.core
-from pygooglehelper import register_functions, ConfigRequest
+from pygooglehelper import register_functions, ConfigRequest, get_credentials
 from pytconf import register_main, config_arg_parse_and_launch, register_endpoint
+import googleapiclient
 
 from pygcal.static import DESCRIPTION, APP_NAME, VERSION_STR
-from pygcal.constants import SCOPES
+from pygcal.constants import SCOPES, API_SERVICE_NAME, API_VERSION
 
 
 @register_endpoint(
     description="List calendars",
 )
 def calendars_list() -> None:
-    pass
+    api = get_api()
+    print("Getting the list of calendars...")
+    # pylint: disable=no-member
+    calendars_result = api.calendarList().list().execute()
+    calendars = calendars_result.get("items", [])
+
+    if not calendars:
+        print("No calendars found.")
+        return
+
+    print("Your Calendars:")
+    for calendar in calendars:
+        summary = calendar.get("summary", "No Title")
+        calendar_id = calendar["id"]
+        print(f"- {summary} (ID: {calendar_id})")
 
 
 @register_main(
@@ -32,3 +47,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def get_api():
+    ConfigRequest.scopes = SCOPES
+    ConfigRequest.app_name = APP_NAME
+    credentials = get_credentials()
+    return googleapiclient.discovery.build(
+        serviceName=API_SERVICE_NAME,
+        version=API_VERSION,
+        credentials=credentials,
+        cache_discovery=False,
+    )
