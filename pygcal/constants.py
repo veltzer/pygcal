c0008424c07672ac28ba1e37e752612d3f9971b9
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-26 09:13:37 +0300

    no commit message given

--- pygcal/constants.py
+++ pygcal/constants.py
@@ -4,6 +4,7 @@ constants.py
 
 SCOPES = [
     "https://www.googleapis.com/auth/calendar",
+    # "https://www.googleapis.com/auth/calendar.readonly",
 ]
 API_SERVICE_NAME = "calendar"
 API_VERSION = "v3"
